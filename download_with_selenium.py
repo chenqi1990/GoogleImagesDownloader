50449f950e20792e9a2ca64ab99860ad7003c794
陈奇 <chenqi.02@bytedance.com>
2018-11-28 15:59:20 +0800

    tuchong supporting

--- download_with_selenium.py
+++ download_with_selenium.py
@@ -33,6 +33,111 @@ def check():
             return False
     return True
 
+def get_image_links_tuchong_community(main_keyword, supplemented_keywords, link_file_path):
+    """get image links with selenium
+    
+    Args:
+        main_keyword (str): main keyword
+        supplemented_keywords (list[str]): list of supplemented keywords
+        link_file_path (str): path of the file to store the links
+        num_requested (int, optional): maximum number of images to download
+    
+    Returns:
+        None
+    """
+    number_of_scrolls = int(num_requested / 400) + 1 
+    # number_of_scrolls * 400 images will be opened in the browser
+
+    img_urls = set()
+    driver = webdriver.Firefox()
+    for i in range(len(supplemented_keywords)):
+        search_query = main_keyword + ' ' + supplemented_keywords[i]
+        url = "https://tuchong.com/search/post/?query="+search_query
+        driver.get(url)
+        
+        for _ in range(number_of_scrolls):
+            for __ in range(10000000):
+                # multiple scrolls needed to show all 400 images
+                driver.execute_script("window.scrollBy(0, 1000)")
+                time.sleep(2)
+                end = driver.find_element_by_xpath('//i[contains(@class, "icon-end")]')
+                if end[0].getAttribute('style') == '':
+                    break
+            time.sleep(5)
+
+        imges = driver.find_elements_by_xpath('//div[@data-lazy-url]')
+        for img in imges:
+            img_url = img.getAttribute('data-lazy-url')
+            if img_url is None:
+                continue
+            # img_type = json.loads(img.get_attribute('innerHTML'))["ity"]
+            img_urls.add('https:' + img_url)
+        print('Process-{0} add keyword {1} , got {2} image urls so far'.format(main_keyword, supplemented_keywords[i], len(img_urls)))
+    print('Process-{0} totally get {1} images'.format(main_keyword, len(img_urls)))
+    driver.quit()
+
+    with open(link_file_path, 'w') as wf:
+        for url in img_urls:
+            wf.write(url +'\n')
+    print('Tuchong: Store all the links in file {0}'.format(link_file_path))
+
+def get_image_links_tuchong(main_keyword, supplemented_keywords, link_file_path):
+    """get image links with selenium
+    
+    Args:
+        main_keyword (str): main keyword
+        supplemented_keywords (list[str]): list of supplemented keywords
+        link_file_path (str): path of the file to store the links
+        num_requested (int, optional): maximum number of images to download
+    
+    Returns:
+        None
+    """
+    img_urls = set()
+    driver = webdriver.Firefox()
+    for i in range(len(supplemented_keywords)):
+        search_query = main_keyword + supplemented_keywords[i]
+        url = "https://stock.tuchong.com/search?term=" + search_query
+        driver.get(url)
+        driver.refresh()
+        for page_id in range(50):
+            print('page_id = %s' % page_id)
+            for __ in range(10):
+                # multiple scrolls needed to show all 400 images
+                driver.execute_script("window.scrollBy(0, 500)")
+                time.sleep(1)
+
+            print("xpath")
+            try:
+                imges = driver.find_elements_by_xpath('//img')
+            except Exception as e:
+                print(e)
+                continue
+            for img in imges:
+                img_url = img.get_attribute('src')
+                if img_url is None or 'pstatp' not in img_url:
+                    continue
+                img_urls.add(img_url)
+            print("img urls = %s" % len(img_urls))
+            # to load next 400 images
+            time.sleep(3)
+            try:
+                print("click");
+                driver.find_element_by_xpath('//a[contains(@class,"page-next")]').click()
+                print("click");
+            except Exception as e:
+                print("Process-{0} reach the end of page or get the maximum number of requested images".format(main_keyword))
+                break
+
+        print('Process-{0} add keyword {1} , got {2} image urls so far'.format(main_keyword, supplemented_keywords[i], len(img_urls)))
+    print('Process-{0} totally get {1} images'.format(main_keyword, len(img_urls)))
+    driver.quit()
+
+    with open(link_file_path, 'w') as wf:
+        for url in img_urls:
+            wf.write(url +'\n')
+    print('Tuchong: Store all the links in file {0}'.format(link_file_path))
+
 
 def get_image_links_google(main_keyword, supplemented_keywords, link_file_path, num_requested = 1000):
     """get image links with selenium
@@ -215,6 +320,10 @@ def main(config_file, topic_name):
                 p.apply_async(get_image_links_bing, args=(keyword, supplemented_keywords, link_file_path))
             elif engine == 'google':
                 p.apply_async(get_image_links_google, args=(keyword, supplemented_keywords, link_file_path))
+            elif engine == 'tuchong':
+                p.apply_async(get_image_links_tuchong, args=(keyword, supplemented_keywords, link_file_path))
+            elif engine == 'tuchong_community':
+                p.apply_async(get_image_links_tuchong_community, args=(keyword, supplemented_keywords, link_file_path))
             else:
                 p.apply_async(get_image_links_baidu, args=(keyword, supplemented_keywords, link_file_path))
         p.close()
